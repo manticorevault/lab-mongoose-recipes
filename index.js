547c4c0f38a85640448a809e46cf90f6d547a92e
Artur Serra <artur.aserra@gmail.com>
2020-05-07 19:40:40 +0100

    Lab done

--- index.js
+++ index.js
@@ -21,6 +21,42 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    console.log("Connect to MongoDB")
+
+    return Recipe.create({
+      title: 'Vegan Kjottbullar',
+      level: 'Easy Peasy',
+      ingredients: ['Vegan meatballs', 'sauce'],
+      cuisine: 'Swedish',
+      dishType: 'main_course',
+      duration: 180,
+      creator: 'Artur Serra'
+    });
+  })
+  .then((recipe) => {
+    console.log("Your recipe was added to the database:", recipe.title)
+
+    return Recipe.insertMany(data);
+  })
+  .then((recipe) => {
+    for(recipeTitle of recipe) {
+      console.log("This recipe is called:", recipeTitle.title);
+    }
+
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100});
+  })
+  .then((recipe) => {
+    console.log("Your recipe was successfully updated!")
+
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then((recipe) => {
+    console.log("Recipe successfully deleted!")
+
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log("Disconnected from MongoDB")
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
